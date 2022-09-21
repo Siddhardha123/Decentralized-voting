fb8b563d43ca5480e1906276b9d6b6ff5d3658d6
siddhardha123 <siddhardha123123@gmail.com>
2022-09-22 01:46:56 +0530

    first

---
+++ doting/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from './components/Login'
+import './components/Login.scss'
+const App = () => {
+  return (
+    <>
+         <Login />
+    </>
+  )
+}
+
+export default App
