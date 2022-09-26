58df3b7ab009c7966d28cecd8d3c830a7af1065f
siddhardha123 <siddhardha123123@gmail.com>
2022-09-26 11:52:38 +0530

    voterjs done

--- doting/context/Voter.js
+++ doting/context/Voter.js
@@ -1,20 +1,58 @@
 import React,{useState,useEffect} from 'react';
 import Web3Modal from 'web3modal';
-import {ethers} from "ethers";
+import ethers from "ethers";
 import {create as ipfsHttpClient} from "ipfs-http-client";
 import axios from 'axios';
-import {useRouter} from "next/router";
-
-import {VotingAddress,VotingAddressABI} from './Constants'
+import useRouter from "next/router";
+import VotingAddress  from './Constants';
+import VotingAddressABI from './Constants';
 const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
 const fetchContract = (signerOrProvider) => new ethers.Contract(VotingAddress,VotingAddressABI,signerOrProvider);
- 
   export const VotingContext = React.createContext();
   export const VotingProvider = ({children}) =>{
+        const router = useRouter;
+        const [currentAccount,setCurrentAccount] = useState('');
+        const [candidateLength,setCandidatelength] = useState('');
+        const pushCandidate = [];
+        const  candidateIndex = [];
+        const [candidateArray,setCandidateArray] = useState(pushCandidate);
+        // candidate data ends 
+        const [error,setError] = useState('');
+        const higestVote = [];
+        // voters section
+        const pushVoter = [];
+        const [voterArray,setVoterArray] = useState(pushVoter);
+        const [voterLength,setVoterLength] = useState('');
+        const [voterAddress,setVoterAddress] = useState([]);
+     // connecting metamask
+     const checkIfWalletIsConnected = async() =>{
+         if(!window.ethereum) return setError("please install metamask")
+         const account = await window.ethereum.request({method:"eth_accounts"})
+         if(account.length){
+            setCurrentAccount(account[0]);
+         } else{
+            setError("Please Install MetaMask and connect , Reload");
+         }
+     };
+     // connect wallet
+     const connectWallet = async()=>{
+        if(!window.ethereum) return setError("please Install metamask")
+        const account = await window.ethereum.request({method:"eth_requestAccounts"});
+        setCurrentAccount(account[0]);
+     }
+     // upload to ipfs 
+       const uploadToIPFS = async(file)=>{
+           try{
+             const added = await client.add({content: file})
+             const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+             return url;
+           }catch(error){
+            setError("error uploading file to ipfs")
+           }
+       }
 
-      const votingTitle = "my first contract"
      return(
-        <VotingContext.Provider value={{votingTitle}}>
+        <VotingContext.Provider value={{checkIfWalletIsConnected,connectWallet,uploadToIPFS}}>
             {children}
         </VotingContext.Provider>
      );
--- doting/pages/index.js
+++ doting/pages/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import {useContext} from 'react';
+import useContext from 'react';
 import Image from 'next/image';
 import Countdown from 'react-countdown';
 
 
 import {VotingContext} from '../context/Voter';
-// import Style from '../styles/index.module.css';
-// import Card from '../components/Card/Card';
+import Style from '../styles/index.module.css';
+import Card from '../components/Card/Card';
 // import image from  "";
 const Home = () => {
   return (
